d2a1ef257e86c04d53eef40469c9407e2ef9fa0f
rikarani <46773591+rikarani@users.noreply.github.com>
2025-02-03 09:19:06 +0700

    feat: add Pagination and Search components for game management

---
+++ src/components/dashboard/manage-game/search.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { FC } from "react";
+
+import { Icon } from "@iconify-icon/react";
+import { Button } from "@heroui/button";
+import { Input, InputProps } from "@heroui/input";
+
+import Link from "next/link";
+
+type Props = Pick<InputProps, "value" | "onValueChange" | "onClear">;
+
+export const Search: FC<Props> = ({ value, onValueChange, onClear }) => {
+  return (
+    <div className="w-full items-center justify-between gap-3 space-y-4 lg:flex lg:space-y-0">
+      <Input
+        isClearable
+        startContent={<Icon icon="lucide:search" className="text-base" />}
+        placeholder="cari game..."
+        value={value}
+        onValueChange={onValueChange}
+        onClear={onClear}
+      />
+      <Button
+        as={Link}
+        className="w-full lg:w-auto"
+        href="/dashboard/admin/manage-game/tambah-game"
+        startContent={<Icon icon="lucide:plus" className="text-base" />}
+        color="primary"
+      >
+        Tambah Game
+      </Button>
+    </div>
+  );
+};
