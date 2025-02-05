c4b0c1f89146904d8675181391b98d4003ddeed3
rikarani <46773591+rikarani@users.noreply.github.com>
2025-02-06 01:02:43 +0700

    feat: conditionally render 'Tambah Game' button based on current pathname

--- src/components/dashboard/manage-game/search.tsx
+++ src/components/dashboard/manage-game/search.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FC } from "react";
+import { usePathname } from "next/navigation";
 
 import { Icon } from "@iconify-icon/react";
 import { Button } from "@heroui/button";
@@ -11,6 +12,8 @@ import Link from "next/link";
 type Props = Pick<InputProps, "value" | "onValueChange" | "onClear">;
 
 export const Search: FC<Props> = ({ value, onValueChange, onClear }) => {
+  const pathname = usePathname();
+
   return (
     <div className="w-full items-center justify-between gap-3 space-y-4 lg:flex lg:space-y-0">
       <Input
@@ -21,15 +24,17 @@ export const Search: FC<Props> = ({ value, onValueChange, onClear }) => {
         onValueChange={onValueChange}
         onClear={onClear}
       />
-      <Button
-        as={Link}
-        className="w-full lg:w-auto"
-        href="/dashboard/admin/manage-game/tambah-game"
-        startContent={<Icon icon="lucide:plus" className="text-base" />}
-        color="primary"
-      >
-        Tambah Game
-      </Button>
+      {pathname === "/dashboard/admin/manage-game" && (
+        <Button
+          as={Link}
+          className="w-full lg:w-auto"
+          href="/dashboard/admin/manage-game/tambah-game"
+          startContent={<Icon icon="lucide:plus" className="text-base" />}
+          color="primary"
+        >
+          Tambah Game
+        </Button>
+      )}
     </div>
   );
 };
