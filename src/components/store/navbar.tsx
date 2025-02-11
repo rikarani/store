3bf989ed2c88164c8ba36f0fcee2dde89e8cad18
rikarani <46773591+rikarani@users.noreply.github.com>
2025-02-11 22:46:08 +0700

    feat: enhance Navbar component with loading states and user profile integration

--- src/components/store/navbar.tsx
+++ src/components/store/navbar.tsx
@@ -1,16 +1,22 @@
+"use client";
+
 import { FC } from "react";
 import Link from "next/link";
 
-import { SignedOut, SignedIn, SignInButton } from "@clerk/nextjs";
+import { SignedOut, SignedIn, SignInButton, useUser, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
 
-import { Brand } from "@/icons";
 import { Profile } from "./profile";
+import { Brand } from "@/icons";
+
 import { ThemeSwitcher } from "@/components/ui/theme-switcher";
 
+import { Skeleton } from "@heroui/skeleton";
 import { Button } from "@heroui/button";
 import { Navbar as Nav, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
 
 export const Navbar: FC = () => {
+  const { user } = useUser();
+
   return (
     <Nav>
       <NavbarBrand as={Link} href="/" className="max-w-max">
@@ -22,16 +28,21 @@ export const Navbar: FC = () => {
           <ThemeSwitcher />
         </NavbarItem>
         <NavbarItem>
-          <SignedOut>
-            <SignInButton>
-              <Button color="primary" size="sm" variant="solid">
-                Login
-              </Button>
-            </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <Profile />
-          </SignedIn>
+          <ClerkLoading>
+            <Skeleton className="size-8 rounded-full" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignInButton>
+                <Button color="primary" size="sm" variant="solid">
+                  Login
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Profile user={user} />
+            </SignedIn>
+          </ClerkLoaded>
         </NavbarItem>
       </NavbarContent>
     </Nav>
