3f0b8e9299f01730406f825da5008a4ef7767951
rikarani <46773591+rikarani@users.noreply.github.com>
2025-02-11 14:51:17 +0700

    tes static

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { FC, Suspense } from "react";
+import { FC } from "react";
 
 import { Navbar } from "@/components/store/navbar";
 import { Banner } from "@/components/store/banner";
 import { AllGame } from "@/components/store/all-game";
 import { OtherGame } from "@/components/store/other-game";
 
-import { Spinner } from "@heroui/spinner";
+// import { Spinner } from "@heroui/spinner";
 import { Divider } from "@heroui/divider";
 
 import suisei from "~/images/suisei.webp";
@@ -23,18 +23,14 @@ const Index: FC = () => {
         <div className="px-6">
           <h2 className="text-xl font-bold">Game Populer</h2>
           <div className="mt-4 grid grid-cols-3 gap-4 sm:grid-cols-4">
-            <Suspense fallback={<Spinner />}>
-              <AllGame />
-            </Suspense>
+            <AllGame />
           </div>
         </div>
         <Divider orientation="vertical" className="h-auto" />
         <div className="px-6">
           <h2 className="text-xl font-bold">Lainnya</h2>
           <div className="mt-4 grid grid-cols-3 gap-4">
-            <Suspense fallback={<Spinner />}>
-              <OtherGame />
-            </Suspense>
+            <OtherGame />
           </div>
         </div>
       </div>
