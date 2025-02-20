58cee36f6b0e9a1720056f1785cbd2dabb7a8cf0
rikarani <46773591+rikarani@users.noreply.github.com>
2025-02-21 02:04:50 +0700

    feat: add metadata generation for game editing page and enhance UI structure

--- app/dashboard/admin/manage-game/[id]/page.tsx
+++ app/dashboard/admin/manage-game/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { FC } from "react";
+import type { Metadata } from "next";
 
 import { PrismaClient } from "@prisma/client";
+
+import { EditGame } from "@/components/dashboard/manage-game/edit-game";
+import { DashboardSection } from "@/components/dashboard/dashboard-section";
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -8,16 +12,31 @@ type Props = {
 
 const prisma = new PrismaClient();
 
-const Page: FC<Props> = async ({ params }) => {
-  const { id } = await params;
-
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const game = await prisma.game.findUnique({
     where: {
-      id,
+      id: (await params).id,
     },
   });
 
-  return <h1>ini game yang namanya {game?.name}</h1>;
+  return {
+    title: `Edit Game ${game?.name} - Erika Store`,
+    description: `Edit Game ${game?.name}`,
+  };
+}
+
+const Page: FC<Props> = async ({ params }) => {
+  const game = await prisma.game.findUnique({
+    where: {
+      id: (await params).id,
+    },
+  });
+
+  return (
+    <DashboardSection name={`Manage Game - ${game?.name || "mak lemak lemak"}`}>
+      <EditGame game={game} />
+    </DashboardSection>
+  );
 };
 
 export default Page;
