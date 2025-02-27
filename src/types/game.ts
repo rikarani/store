960e7d597521a088903788e657b970bc889a2587
rikarani <46773591+rikarani@users.noreply.github.com>
2025-02-27 15:09:50 +0700

    refactor: enhance game type definitions with API integration

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,4 +1,13 @@
-export type MutateGameState = {
+import type { Game } from "@prisma/client";
+
+type MutateGameState = {
   success: boolean;
   message: string;
 };
+
+type GameFromAPI = Pick<Game, "name" | "code"> & {
+  active: boolean;
+  icon_url: string;
+};
+
+export type { MutateGameState, GameFromAPI };
