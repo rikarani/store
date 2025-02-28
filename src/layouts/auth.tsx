3af15056c61c26ae2f435b2264632e7af00d7b59
rikarani <46773591+rikarani@users.noreply.github.com>
2025-03-01 03:05:47 +0700

    feat: replace AuthSection with AuthLayout for improved layout consistency in authentication pages

---
+++ src/layouts/auth.tsx
@@ -0,0 +1,23 @@
+import { FC, PropsWithChildren } from "react";
+
+import { Divider } from "@heroui/divider";
+
+type Props = {
+  title: string;
+  subtitle: string;
+};
+
+export const AuthLayout: FC<PropsWithChildren<Props>> = ({ title, subtitle, children }) => {
+  return (
+    <div className="w-full max-w-screen-md px-6 py-4">
+      <div className="w-full">
+        <p className="text-xl font-semibold">{title}</p>
+        <p className="text-sm font-medium text-default-500">{subtitle}</p>
+      </div>
+      <div className="py-4">
+        <Divider />
+      </div>
+      {children}
+    </div>
+  );
+};
