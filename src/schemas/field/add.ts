ca2e380594ae871abd31472b9c20f503508ef200
rikarani <46773591+rikarani@users.noreply.github.com>
2025-03-02 17:58:23 +0700

    feat: add validation schema for adding new fields using Zod

--- src/schemas/field/add.ts
+++ src/schemas/field/add.ts
@@ -1,1 +1,11 @@
-// import { z } from "zod";
+import { z } from "zod";
+
+const addFieldSchema = z.object({
+  game_id: z.string().uuid().trim(),
+  name: z.string().trim(),
+  label: z.string().trim(),
+  type: z.enum(["text", "dropdown"]),
+});
+
+export { addFieldSchema };
+export type AddFieldSchema = z.infer<typeof addFieldSchema>;
